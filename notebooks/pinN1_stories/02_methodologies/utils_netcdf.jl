d62bc89db4d957b303dec7f5e3a45e9a16cedf11
biothomme <55461592+biothomme@users.noreply.github.com>
2022-08-29 17:23:32 +0200

    added deep copy for regridders

--- notebooks/pinN1_stories/02_methodologies/utils_netcdf.jl
+++ notebooks/pinN1_stories/02_methodologies/utils_netcdf.jl
@@ -65,8 +65,10 @@ function extract_subarray(data::NCDatasets.CFVariable,
         dimension_vectors::AbstractArray, dest_itvs::AbstractArray)
     index_wghts = []
     for (dim_itvs, dest_itv) in zip(dimension_vectors, dest_itvs)
+        println(dim_itvs, dest_itv)
         push!(index_wghts, intsect_intervals(dim_itvs, dest_itv))
     end
+    println(index_wghts)
     indices = map(j -> convert(Array{Int}, index_wghts[j][1,:]),
             1:length(index_wghts))
     wghts = map(j -> index_wghts[j][2,:], 1:length(index_wghts))
@@ -122,7 +124,7 @@ The output is a dictionary that maps axes name to its
 intervals.
 """
 function dimwise_intervals(regridder::RG) where RG<:Regridder
-    Dict(x.name => intervals(x) for x in regridder.axes)
+    Dict(x.name => intervals(x) for x in values(regridder.axes))
 end
 
 
@@ -164,4 +166,72 @@ vectors in `vectorbases` with each other.
 function vectors_product(vectorbases::AbstractArray)
     flat_tensor = reduce(colrowvector_multiply, vectorbases)
     reshape(flat_tensor, (length.(vectorbases)...))
+end
+
+"""
+    deepcopy(rga::RegridAxis) -> RegridAxis
+
+Make deep copy if `RegridAxis`.
+"""
+function Base.deepcopy(rga::RegridAxis)
+    RegridAxis(rga.name, rga.coordmin, rga.coordmax, rga.coordcard)
+end
+
+"""
+    deepcopy(rgd::Regridder) -> Any
+
+Make deep copy if `Regridder` subtype.
+"""
+function Base.deepcopy(rgd::RG) where RG<:Regridder
+    newaxes = map(deepcopy, rgd.axes)
+    Regridder(newaxes)
+end
+
+"""
+    alter_regridder3D(rgd::RG; latcard, loncard, indcard, latmin,
+        latmax, latname, lonmin, lonmax, lonname, indmax, indmin,
+        indname)
+
+Make an altered deeep copy of a `Regridder3D` instance.
+"""
+function alter_regridder3D(rgd::RG; latcard=nothing, loncard=nothing, indcard=nothing, latmin=nothing,
+        latmax=nothing, latname=nothing, lonmin=nothing,
+        lonmax=nothing, lonname=nothing, indmax=nothing,
+        indmin=nothing, indname=nothing) where RG<:Regridder
+    
+    # fill missing args from input regridder
+    function fillfromregridder!(rgdder, axdict, axname, newdict)
+        ax = rgdder.axes[axname]
+        newaxdict = Dict()
+        newaxdict["card"] = axdict["card"]===nothing ? ax.coordcard : axdict["card"]
+        newaxdict["min"] = axdict["min"]===nothing ? ax.coordmin : axdict["min"]
+        newaxdict["max"] = axdict["max"]===nothing ? ax.coordmax : axdict["max"]
+        newaxdict["name"] = axdict["name"]===nothing ? ax.name : axdict["name"]
+        newdict[axname] = newaxdict
+    end 
+    
+    # convert all numbers to same types
+    function promoteall!(fullaxesdict, defaultcard, defaultcoord)
+        fullaxesdict["min"] = convert(defaultcoord, fullaxesdict["min"])
+        fullaxesdict["max"] = convert(defaultcoord, fullaxesdict["max"])
+
+        fullaxesdict["card"] = convert(defaultcard, fullaxesdict["card"])
+    end
+    
+    axesvarsplit = Dict("lat" => Dict("card"=>latcard, "min"=>latmin, "max"=>latmax, "name"=>latname),
+                        "lon" => Dict("card"=>loncard, "min"=>lonmin, "max"=>lonmax, "name"=>lonname),
+                        "ind" => Dict("card"=>indcard, "min"=>indmin, "max"=>indmax, "name"=>indname))
+    newaxesvarsplit = Dict()
+
+    kys = collect(keys(axesvarsplit))
+    map(x -> fillfromregridder!(rgd, axesvarsplit[x], x, newaxesvarsplit), kys)
+
+    card_default = typeof(newaxesvarsplit["lat"]["card"])
+    coord_default = typeof(newaxesvarsplit["lat"]["max"])
+    map(x -> promoteall!(newaxesvarsplit[x], card_default, coord_default), kys)
+    latdict, londict, inddict = newaxesvarsplit["lat"], newaxesvarsplit["lon"], newaxesvarsplit["ind"]
+    Regridder3D(latdict["card"], londict["card"], inddict["card"]; 
+        latmin=latdict["min"], latmax=latdict["max"], latname=latdict["name"], 
+        lonmin=londict["min"], lonmax=londict["max"], lonname=londict["name"],
+        indmin=inddict["min"], indmax=inddict["max"], indname=inddict["name"])
 end
