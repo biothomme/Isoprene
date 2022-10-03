a61dfaa027160815699591d8379ef423207886b0
biothomme <55461592+biothomme@users.noreply.github.com>
2022-10-03 15:39:49 +0200

    init; script to regrid the datasets obtained from griddingmachine.jl

---
+++ src/scripts/python/02_regrid_griddinmachine_datasets.py
@@ -0,0 +1,128 @@
+# IMPORTS
+from functools import reduce
+import os
+import sys
+import yaml
+from inspect import getsourcefile
+
+
+# PREAMBLE
+PATHS_FILE = os.path.join(
+    os.path.dirname(os.path.abspath(getsourcefile(lambda:0))),
+    "../../../PATHS.yml")   # this is a method flexible to exectuion location
+PCAT = yaml.load(open(PATHS_FILE, "r"), Loader=yaml.FullLoader)
+
+
+# UTIL
+def get_path(*key_args):
+    """get_path
+
+    Convert path from catalogue file to absolute path for executing file.
+
+    Returns:
+        _type_: _description_
+    """
+    path = reduce(lambda x, y: x.get(y), key_args, PCAT)
+    return os.path.abspath(os.path.join(os.path.dirname(PATHS_FILE), path))
+
+
+# FUNCTIONS OF MAIN
+def access_src_code():
+    """access_src_code 
+
+    Acces the directory with the Regridder classes.
+    """
+    DIR_METHODS = get_path("CODE", "METHODOLOGIES", "ROOT")
+    sys.path.insert(1, DIR_METHODS)
+    return
+
+def split_datasets():
+    """split_datasets 
+
+    Group the GriddingMachine.jl datasets respective different ways of regridding.
+
+    Returns:
+        dict: Dictionary grouping the different datasets.
+    """
+    import xarray as xr
+    
+    dict_gmfeatures_grouped = {
+        "temporal": {},
+        "categorical": {},
+        "multilayered": {},
+        "simple": {}
+    }
+    dict_gmfeatures = {
+        k: v for k, v
+        in PCAT["DATA"]["FEATURES"].items()
+        if "GRIDDING_MACHINE" in v}
+    
+    # for different kind of datasets we apply other regriddings
+    # thus, we split them into 4 groups
+    for k, v in dict_gmfeatures.items():
+        # most complex are data with a time component
+        if not "_1Y_" in v["GRIDDING_MACHINE"]:
+            group = "temporal"
+        # most simple those with only lat and lon
+        elif len(xr.open_dataset(
+            get_path("DATA", "FEATURES", k, "GRIDDING_MACHINE")).sizes) == 2:
+            group = "simple"
+        # also we have PFT as mulilayered catergorical data
+        elif k == "PLANT_FUNC_TYPE":
+            group = "categorical"
+        # and finally multilayered soil data
+        else:
+            group = "multilayered"
+        dict_gmfeatures_grouped[group].update({k: v})
+    return dict_gmfeatures_grouped
+
+def get_regridder(name_ncfile):
+    """get_regridder
+
+    Make regridder instance for given file name.
+
+    Args:
+        name_ncfile (str): Path of netCDF file.
+
+    Returns:
+        regridder.RegridderSimple: Wrapper for regridding
+    """
+    from regridder import RegridderSimple
+    return RegridderSimple(name_ncfile)
+
+def download_era5(cdsapi_fetcher):
+    # define the csv file for subareas, the output directory, as well as the
+    # csv file that tracks the progress.
+    FILE_SUBAREAS_V1 = get_path("DATA", "FEATURES", "SUBAREASELECTION", "V01")
+    DIR_ERA5_V1 = get_path("DATA", "FEATURES", "ERA5HOURLY", "V01", "ROOT")
+    FILE_LOG = get_path("DATA", "FEATURES", "ERA5HOURLY", "V01", "LOG")
+
+    # download the data
+    cdsapi_fetcher.get_data(
+        FILE_SUBAREAS_V1, directory=DIR_ERA5_V1, name_logcsvfile=FILE_LOG)
+    return
+
+
+# MAIN
+def main():
+    # establish access to src code
+    access_src_code()
+
+    # group all datasets
+    dict_datasets = split_datasets()
+    
+    # first we make the simple bilinear non temporal scalar regridding
+    for key in dict_datasets["simple"].keys():
+        file_ds = get_path("DATA", "FEATURES", key, "GRIDDING_MACHINE")
+        file_ds_regridded = get_path("DATA", "FEATURES", key, "V01")
+        
+        rgs = RegridderSimple(file_ds)
+        rgs.regrid_and_save(FILE_OUTPUT)
+
+    print("Done!")
+    return
+
+
+# RUN
+if __name__ == "__main__":
+    main()
